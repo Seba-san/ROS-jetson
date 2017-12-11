d6118918b609e6fe88a74e74b680b63835b493c1
Seba-san <sansoni.sebas@gmail.com>
2017-12-11 17:24:02 -0300

    Algunos errores corregidos

--- Jetson/iniciarentornoros.sh
+++ Jetson/iniciarentornoros.sh
@@ -10,6 +10,12 @@ cd ~/catkin_ws
 source devel/setup.bash
 echo $ROS_PACKAGE_PATH
 export ROS_MASTER_URI=http://$master:11311
-echo "ejecute el siguiente comando:"
-echo -e "export ROS_IP=\e[1;32m\$jetson_xxx \e[0m"
-echo -e "poniendo \e[1;31m\$jetson_eth \e[0m si se conecta via eth o \e[1;31m\$jetson_wlan \e[0m por wlan0  "
+if [-n $jetson]
+then
+  export ROS_IP=$jetson
+else
+  echo "ejecute el siguiente comando:"
+  echo -e "export ROS_IP=\e[1;32m\$jetson_xxx \e[0m"
+  echo -e "poniendo \e[1;31m\$jetson_eth \e[0m si se conecta via eth o \e[1;31m\$jetson_wlan \e[0m por wlan0  "
+
+fi
