86c05b296600d1a4a107361ece4fd555a5f7eee1
Seba-san <sansoni.sebas@gmail.com>
2017-12-11 16:50:50 -0300

    se realizaron algunos ajustes previo al testeo

--- Jetson/conexion.sh
+++ Jetson/conexion.sh
@@ -5,7 +5,9 @@ jetson_eth=$(ifconfig eth0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep 
 jetson_wlan=$(ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 #busca en los registros la ultima IP conectada
 master=$(sudo cat /var/log/auth* | grep Accepted | awk 'NR==1 {print $11  }') # busca el registro de conexiones y se queda con la ultima
-
+#resultado=$master && 255.255.255.0
+#echo "$resultado"
+# ¿Donde esta conectado via eth o via wlan?
 echo "master $master"
 echo "jetson_eth $jetson_eth"
 echo "jetson_wlan $jetson_wlan"
--- Jetson/iniciarentornoros.sh
+++ Jetson/iniciarentornoros.sh
@@ -5,8 +5,11 @@ source ./conexion.sh
 cd ~
 source /opt/ros/kinetic/setup.sh
 echo "Se asume que el workspace esta en ~/catkin_ws"
+# NO ESTA DECLARADO ROS_WORKSPACE :(
 cd ~/catkin_ws
 source devel/setup.bash
 echo $ROS_PACKAGE_PATH
 export ROS_MASTER_URI=http://$master:11311
-export ROS_IP=$jetson
+echo "ejecute el siguiente comando:"
+echo -e "export ROS_IP=\e[1;32m\$jetson_xxx \e[0m"
+echo -e "poniendo \e[1;31m\$jetson_eth \e[0m si se conecta via eth o \e[1;31m\$jetson_wlan \e[0m por wlan0  "
